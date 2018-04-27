09d38ca075b7aca206556bf2c91025e308b2d889
Alex Spirgel <spirgel.alexander@gmail.com>
2018-04-27 01:18:36 -0400

    Updated script to better handle null and added comments

--- extend.js
+++ extend.js
@@ -1,51 +1,56 @@
 /**
- * object_extend v1.0.0
+ * extend v1.1.0
  * https://github.com/alexspirgel/extend
  */
 
 /**
  * Extends an object with another other object.
  *
- * @param {object} target_object The object that will receive the new properties.
- * @param {object|array} merge_objects An object or array of objects containing properties to be merged into the target object.
- * @param {boolean} [deep] If true, the merge becomes recursive (aka. deep copy).
+ * @param {object} target_object The object that will receive the new properties and values.
+ * @param {object|array} merge_objects An object or array of objects to be merged into the target_object.
+ * @param {boolean} [deep] If true, the extend becomes recursive (aka. deep clone).
+ *
  * @return {object} The extended target_object.
  */
 
-var extend_object = function (target_object, merge_objects, deep) {
+var extend = function (target_object, merge_objects, deep) {
 	'use strict';
 
-	var extend = function (target_object, merge_object, deep) {
-		for(var key in merge_object) {
-			if(typeof merge_object[key] === 'object') {
-				// If the property value is an array.
-				if(Array.isArray(merge_object[key])) {
-					// Set the target property value equal to an empty array (arrays are replaced, not merged).
-					target_object[key] = [];
+	var extend_object = function (target_object, merge_object, deep) {
+		// For each property in the merge_object.
+		for(var property in merge_object) {
+			// If the current property value is an object, is not null, and the deep flag is true.
+			if(typeof merge_object[property] === 'object' && merge_object[property] !== null && deep) {
+				// If the current property value is an array.
+				if(Array.isArray(merge_object[property])) {
+					// Set the target_object property value equal to an empty array (arrays are replaced, not merged).
+					target_object[property] = [];
 				}
-				// If the target property is not an object or it is not a deep clone.
-				else if(typeof target_object[key] !== 'object' || !deep) {
-					// Set the target property value equal to an empty object.
-					target_object[key] = {};
+				// If the target_object property value is not an object or if it is null.
+				else if(typeof target_object[property] !== 'object' || target_object[property] === null) {
+					// Set the target_object property value equal to an empty object.
+					target_object[property] = {};
 				}
-				extend(target_object[key], merge_object[key], deep);
-				// Continue to the next property (prevent copying over the entire object reference).
+				// Call the extend_object function recursively.
+				extend_object(target_object[property], merge_object[property], deep);
+				// Continue to the next property, skipping the normal value assignment.
 				continue;
 			}
-			// If the property was not inherited.
-			if(merge_object.hasOwnProperty(key)){
-				target_object[key] = merge_object[key];
-			}
+			// Set the target_object property value equal to the merge_object property value (for primitive values or shallow calls only).
+			target_object[property] = merge_object[property];
 		}
-
+		// Return the target_object.
 		return target_object;
 	};
 
 	// If merge_objects is not an array, make it an array.
 	merge_objects = Array.isArray(merge_objects) ? merge_objects : [merge_objects];
+	// For each object in merge_objects.
 	for(var object = 0; object < merge_objects.length; object++) {
-		extend(target_object, merge_objects[object], deep);
+		// Extend the target_object with the merge_object.
+		extend_object(target_object, merge_objects[object], deep);
 	}
 
+	// Return the extended target_object.
 	return target_object;
 };
